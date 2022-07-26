f74198e77fcd3de8d59b830a67728587b2bed509
SysWOW64UwU <81893400+SysWOW64UwU@users.noreply.github.com>
2022-07-26 21:15:04 +0530

    Update Start.py

--- InternFreak/Plugins/Start.py
+++ InternFreak/Plugins/Start.py
@@ -3,4 +3,4 @@ from .__init__ import *
 
 @bot.on_message(filters.command(['start', 'start@internfreakbot']) & filters.private)
 def command3(app, message):
-    app.send_message(message.chat.id, "<code>I am a Alive :)</code>")
+    app.send_message(message.chat.id, "<code>I am Alive :)</code>")
