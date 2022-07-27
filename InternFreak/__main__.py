843196549fdc75936f12db7b1552b5d555eb00f6
Mohd Zaid <bzatch70@gmail.com>
2022-07-27 13:40:05 +0530

    Minor Bug Fixed

--- InternFreak/__main__.py
+++ InternFreak/__main__.py
@@ -1,9 +1,3 @@
 from InternFreak import bot, loop
 
-if __name__ == '__main__':
-    loop.create_task(bot.run())
-    try:
-        loop.run_forever()
-    except (KeyboardInterrupt, SystemExit):
-        loop.run_until_complete(bot.stop())
-        loop.close()
+bot.run()
